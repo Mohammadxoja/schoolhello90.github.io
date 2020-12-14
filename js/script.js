fd3cc9b9e2f2f3f452c36f2f678531e4cf17482c
Mohammadxoja <75994491+Mohammadxoja@users.noreply.github.com>
2020-12-14 21:02:29 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// window.onload = function() {
+  
+//   const hover_q = document.querySelector('.hover_q'); 
+//   const q = document.querySelector('.q'); 
+//   const body = document.querySelector('body'); 
+//   hover_q.addEventListener('mousemove', () => {
+//     q.style.opacity = '1'; 
+//     q.style.transition = '0.4s';
+//   }); 
+
+// }
